1d341defe177503b1a11288cdc3da144d6efd325
Tyler Kirumba <Tyler>
2023-03-15 11:04:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+// num1 = 2;
+// num2 = 31
+// const multiply = num1*num2;
+// console.log(multiply);
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+console.log(multiply); // Output: 62
+
+const random = Math.floor(Math.random()) + 1;
+
+// num3 = 32
+// num4 = 7
+
+// const mod = num3 % num4;
+
+// console.log(mod);// Output:4
+
+const num3 = 18;
+const num4 = 7;
+
+const mod = num3 % num4;
+
+console.log(mod); // Output: 4
+
+const max = Math.max(5,10,15,20);
+console.log(max)
